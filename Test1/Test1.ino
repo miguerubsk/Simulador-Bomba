6f8f9f76b73a36b59c9fd2d7426ac9ebd47bc606
Miguerubsk <miguerubsk@gmail.com>
2023-01-04 16:56:09 +0100

    Update Test1.ino

--- Test1/Test1.ino
+++ Test1/Test1.ino
@@ -21,57 +21,35 @@
  * 
  */
 
-#include <SPI.h>          // incluye libreria bus SPI
-#include <MFRC522.h>      // incluye libreria especifica para MFRC522
+#include <SPI.h>      // incluye libreria bus SPI
+#include <MFRC522.h>  // incluye libreria especifica para MFRC522
 
-#define RST_PIN  9      // constante para referenciar pin de reset
-#define SS_PIN  10      // constante para referenciar pin de slave select
-#define TIEMPO_MAX 100   //Constante que determina el temporizador de la bomba
+#define RST_PIN 9       // constante para referenciar pin de reset
+#define SS_PIN 10       // constante para referenciar pin de slave select
+#define TIEMPO_MAX 100  //Constante que determina el temporizador de la bomba
 
 MFRC522 mfrc522(SS_PIN, RST_PIN); // crea objeto mfrc522 enviando pines de slave select y reset
 
-byte LecturaUID[4]; // crea array para almacenar el UID leido
-byte activador[4] = {0x63, 0x7B, 0xEB, 0xA0}; // UID del activador
-byte desactivador[4] = {0x73, 0x35, 0xB0, 0x0E}; // UID del desactivador
+uint8_t LecturaUID[4]; // crea array para almacenar el UID leido
+uint8_t activador[4] = {0x63, 0x7B, 0xEB, 0xA0}; // UID del activador
+uint8_t desactivador[4] = {0x73, 0x35, 0xB0, 0x0E}; // UID del desactivador
 int tiempo; //Tiempo en segundos para la explosión
 boolean activa = false; //Estado por defecto de la bomba
+int ini, fin;
 
 void setup() {
     Serial.begin(9600); // inicializa comunicacion por monitor serie a 9600 bps
     SPI.begin(); // inicializa bus SPI
     mfrc522.PCD_Init(); // inicializa modulo lector
     tiempo = TIEMPO_MAX;
+    ini = millis();
     Serial.println("Listo");
 }
 
 void loop() {
 
-    if (!mfrc522.PICC_IsNewCardPresent())
-        return;
-
-    if (!mfrc522.PICC_ReadCardSerial())
-        return;
-
-    Serial.print("UID:");
-    for (int i = 0; i < mfrc522.uid.size; i++) {
-        if (mfrc522.uid.uidByte[i] < 0x10) {
-            Serial.print(" 0");
-        } else {
-            Serial.print(" ");
-        }
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
-        LecturaUID[i] = mfrc522.uid.uidByte[i];
-    }
-    Serial.print("\n");
-    mfrc522.PICC_HaltA();
-    
-    
-    //if (hayTarjeta) {
-        int comprobacion = comprobarUID(LecturaUID);
-        Serial.print("Comprobación: ");
-        Serial.print(comprobacion);
-        Serial.print("\n");
-        switch (comprobacion) {
+    if (NFCRead()) {
+        switch (comprobarUID(LecturaUID)) {
             case 1:
                 activa = true;
                 break;
@@ -81,22 +59,25 @@ void loop() {
             default:
                 break;
         }
-    //}
-    // if (activa) {
-    //     if (tiempo <= 0) {
-    //         activa = false;
-    //         Serial.println("BUUUUM!!!!!");
-    //         tiempo = TIEMPO_MAX;
-    //     } else {
-    //         Serial.println(tiempo--);
-    //         delay(1000);
-    //     }
+    }
+    if (activa) {
+        if (tiempo <= 0) {
+            activa = false;
+            Serial.println("BUUUUM!!!!!");
+            tiempo = TIEMPO_MAX;
+        } else {
+            fin = millis();
+            if (fin - ini >= 1000) {
+                Serial.println(tiempo--);
+                ini = fin;
+            }
+        }
 
-    // }
-
+    }
 }
 
 bool NFCRead() {
+    memset(LecturaUID, 0, sizeof (uint8_t)*4);
     if (!mfrc522.PICC_IsNewCardPresent())
         return false;
 
@@ -121,11 +102,11 @@ bool NFCRead() {
 int comprobarUID(byte lectura[]) {
     bool esActivador = true;
     bool esDesactivador = true;
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
+    for (uint8_t i = 0; i < mfrc522.uid.size; i++) {
         if (lectura[i] != activador[i])
             esActivador = false;
     }
-    for (byte i = 0; i < mfrc522.uid.size; i++) {
+    for (uint8_t i = 0; i < mfrc522.uid.size; i++) {
         if (lectura[i] != activador[i])
             esDesactivador = false;
     }
