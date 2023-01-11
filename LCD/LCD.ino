df9c950c80642c45d0e1d1f6c48e691844df4d85
Miguerubsk <miguerubsk@gmail.com>
2023-01-11 10:57:02 +0100

    Update LCD.ino
    Primer programa de prueba para el lcd

--- LCD/LCD.ino
+++ LCD/LCD.ino
@@ -1,9 +1,26 @@
+
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x3F,16,2);
+
 void setup() {
   // put your setup code here, to run once:
-
+  // Inicializar el LCD
+  lcd.init();
+  
+  //Encender la luz de fondo.
+  lcd.backlight();
+  
+  // Escribimos el Mensaje en el LCD.
+  lcd.print("Fran Marica");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  lcd.setCursor(0, 1);
+   // Escribimos el número de segundos trascurridos
+  lcd.print(millis()/1000);
+  lcd.print(" Segundos");
+  delay(100);
 }
